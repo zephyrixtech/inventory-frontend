b250900f8bf0e9e0134683a9c6a7f02e07b62cb5
fasilfaz <fazsby2001@gmail.com>
2026-07-08 17:13:53 +0530

    invoice table and currency updated

--- src/pages/invocie/config/InvoicePrintTemplate.tsx
+++ src/pages/invocie/config/InvoicePrintTemplate.tsx
@@ -1,6 +1,17 @@
 import { format } from 'date-fns';
 
-const uaeDirhamSvg = `<svg viewBox="0 0 1000 870" style="height: 10px; width: auto; display: inline-block; vertical-align: middle; fill: #000; margin-left: 2px; margin-right: 2px;"><path d="m88.3 1c0.4 0.6 2.6 3.3 4.7 5.9 15.3 18.2 26.8 47.8 33 85.1 4.1 24.5 4.3 32.2 4.3 125.6v87h-41.8c-38.2 0-42.6-0.2-50.1-1.7-11.8-2.5-24-9.2-32.2-17.8-6.5-6.9-6.3-7.3-5.9 13.6 0.5 17.3 0.7 19.2 3.2 28.6 4 14.9 9.5 26 17.8 35.9 11.3 13.6 22.8 21.2 39.2 26.3 3.5 1 10.9 1.4 37.1 1.6l32.7 0.5v43.3 43.4l-46.1-0.3-46.3-0.3-8-3.2c-9.5-3.8-13.8-6.6-23.1-14.9l-6.8-6.1 0.4 19.1c0.5 17.7 0.6 19.7 3.1 28.7 8.7 31.8 29.7 54.5 57.4 61.9 6.9 1.9 9.6 2 38.5 2.4l30.9 0.4v89.6c0 54.1-0.3 94-0.8 100.8-0.5 6.2-2.1 17.8-3.5 25.9-6.5 37.3-18.2 65.4-35 83.6l-3.4 3.7h169.1c101.1 0 176.7-0.4 187.8-0.9 19.5-1 63-5.3 72.8-7.4 3.1-0.6 8.9-1.5 12.7-2.1 8.1-1.2 21.5-4 40.8-8.9 27.2-6.8 52-15.3 76.3-26.1 7.6-3.4 29.4-14.5 35.2-18 3.1-1.8 6.8-4 8.2-4.7 3.9-2.1 10.4-6.3 19.9-13.1 4.7-3.4 9.4-6.7 10.4-7.4 4.2-2.8 18.7-14.9 25.3-21 25.1-23.1 46.1-48.8 62.4-76.3 2.3-4 5.3-9 6.6-11.1 3.3-5.6 16.9-33.6 18.2-37.8 0.6-1.9 1.4-3.9 1.8-4.3 2.6-3.4 17.6-50.6 19.4-60.9 0.6-3.3 0.9-3.8 3.4-4.3 1.6-0.3 24.9-0.3 51.8-0.1 53.8 0.4 53.8 0.4 65.7 5.9 6.7 3.1 8.7 4.5 16.1 11.2 9.7 8.7 8.8 10.1 8.2-11.7-0.4-12.8-0.9-20.7-1.8-23.9-3.4-12.3-4.2-14.9-7.2-21.1-9.8-21.4-26.2-36.7-47.2-44l-8.2-3-33.4-0.4-33.3-0.5 0.4-11.7c0.4-15.4 0.4-45.9-0.1-61.6l-0.4-12.6 44.6-0.2c38.2-0.2 45.3 0 49.5 1.1 12.6 3.5 21.1 8.3 31.5 17.8l5.8 5.4v-14.8c0-17.6-0.9-25.4-4.5-37-7.1-23.5-21.1-41-41.1-51.8-13-7-13.8-7.2-58.5-7.5-26.2-0.2-39.9-0.6-40.6-1.2-0.6-0.6-1.1-1.6-1.1-2.4 0-0.8-1.5-7.1-3.5-13.9-23.4-82.7-67.1-148.4-131-197.1-8.7-6.7-30-20.8-38.6-25.6-3.3-1.9-6.9-3.9-7.8-4.5-4.2-2.3-28.3-14.1-34.3-16.6-3.6-1.6-8.3-3.6-10.4-4.4-35.3-15.3-94.5-29.8-139.7-34.3-7.4-0.7-17.2-1.8-21.7-2.2-20.4-2.3-48.7-2.6-209.4-2.6-135.8 0-169.9 0.3-169.4 1zm330.7 43.3c33.8 2 54.6 4.6 78.9 10.5 74.2 17.6 126.4 54.8 164.3 117 3.5 5.8 18.3 36 20.5 42.1 10.5 28.3 15.6 45.1 20.1 67.3 1.1 5.4 2.6 12.6 3.3 16 0.7 3.3 1 6.4 0.7 6.7-0.5 0.4-100.9 0.6-223.3 0.5l-222.5-0.2-0.3-128.5c-0.1-70.6 0-129.3 0.3-130.4l0.4-1.9h71.1c39 0 78 0.4 86.5 0.9zm297.5 350.3c0.7 4.3 0.7 77.3 0 80.9l-0.6 2.7-227.5-0.2-227.4-0.3-0.2-42.4c-0.2-23.3 0-42.7 0.2-43.1 0.3-0.5 97.2-0.8 227.7-0.8h227.2zm-10.2 171.7c0.5 1.5-1.9 13.8-6.8 33.8-5.6 22.5-13.2 45.2-20.9 62-3.8 8.6-13.3 27.2-15.6 30.7-1.1 1.6-4.3 6.7-7.1 11.2-18 28.2-43.7 53.9-73 72.9-10.7 6.8-32.7 18.4-38.6 20.2-1.2 0.3-2.5 0.9-3 1.3-0.7 0.6-9.8 4-20.4 7.8-19.5 6.9-56.6 14.4-86.4 17.5-19.3 1.9-22.4 2-96.7 2h-76.9v-129.7-129.8l220.9-0.4c121.5-0.2 221.6-0.5 222.4-0.7 0.9-0.1 1.8 0.5 2.1 1.2z"/></svg>`;
+const getCurrencySvg = (currency: string = 'AED', fill: string = '#000') => {
+  const upper = currency.toUpperCase();
+  if (upper.includes('INR') || upper.includes('₹') || upper.includes('RUPEE')) {
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24" fill="none" stroke="${fill}" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" style="height: 10px; width: auto; display: inline-block; vertical-align: middle; margin-left: 2px; margin-right: 2px;"><path d="M6 3h12M6 8h12M6 13l8.5 8M6 13h3M9 13c6.667 0 6.667-10 0-10"/></svg>`;
+  }
+  if (upper.includes('USD') || upper.includes('$')) {
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24" fill="none" stroke="${fill}" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" style="height: 10px; width: auto; display: inline-block; vertical-align: middle; margin-left: 2px; margin-right: 2px;"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>`;
+  }
+  // Default AED (UAE Dirham)
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="11.5" height="10" viewBox="0 0 1000 870" style="height: 10px; width: auto; display: inline-block; vertical-align: middle; fill: ${fill}; margin-left: 2px; margin-right: 2px;"><path d="m88.3 1c0.4 0.6 2.6 3.3 4.7 5.9 15.3 18.2 26.8 47.8 33 85.1 4.1 24.5 4.3 32.2 4.3 125.6v87h-41.8c-38.2 0-42.6-0.2-50.1-1.7-11.8-2.5-24-9.2-32.2-17.8-6.5-6.9-6.3-7.3-5.9 13.6 0.5 17.3 0.7 19.2 3.2 28.6 4 14.9 9.5 26 17.8 35.9 11.3 13.6 22.8 21.2 39.2 26.3 3.5 1 10.9 1.4 37.1 1.6l32.7 0.5v43.3 43.4l-46.1-0.3-46.3-0.3-8-3.2c-9.5-3.8-13.8-6.6-23.1-14.9l-6.8-6.1 0.4 19.1c0.5 17.7 0.6 19.7 3.1 28.7 8.7 31.8 29.7 54.5 57.4 61.9 6.9 1.9 9.6 2 38.5 2.4l30.9 0.4v89.6c0 54.1-0.3 94-0.8 100.8-0.5 6.2-2.1 17.8-3.5 25.9-6.5 37.3-18.2 65.4-35 83.6l-3.4 3.7h169.1c101.1 0 176.7-0.4 187.8-0.9 19.5-1 63-5.3 72.8-7.4 3.1-0.6 8.9-1.5 12.7-2.1 8.1-1.2 21.5-4 40.8-8.9 27.2-6.8 52-15.3 76.3-26.1 7.6-3.4 29.4-14.5 35.2-18 3.1-1.8 6.8-4 8.2-4.7 3.9-2.1 10.4-6.3 19.9-13.1 4.7-3.4 9.4-6.7 10.4-7.4 4.2-2.8 18.7-14.9 25.3-21 25.1-23.1 46.1-48.8 62.4-76.3 2.3-4 5.3-9 6.6-11.1 3.3-5.6 16.9-33.6 18.2-37.8 0.6-1.9 1.4-3.9 1.8-4.3 2.6-3.4 17.6-50.6 19.4-60.9 0.6-3.3 0.9-3.8 3.4-4.3 1.6-0.3 24.9-0.3 51.8-0.1 53.8 0.4 53.8 0.4 65.7 5.9 6.7 3.1 8.7 4.5 16.1 11.2 9.7 8.7 8.8 10.1 8.2-11.7-0.4-12.8-0.9-20.7-1.8-23.9-3.4-12.3-4.2-14.9-7.2-21.1-9.8-21.4-26.2-36.7-47.2-44l-8.2-3-33.4-0.4-33.3-0.5 0.4-11.7c0.4-15.4 0.4-45.9-0.1-61.6l-0.4-12.6 44.6-0.2c38.2-0.2 45.3 0 49.5 1.1 12.6 3.5 21.1 8.3 31.5 17.8l5.8 5.4v-14.8c0-17.6-0.9-25.4-4.5-37-7.1-23.5-21.1-41-41.1-51.8-13-7-13.8-7.2-58.5-7.5-26.2-0.2-39.9-0.6-40.6-1.2-0.6-0.6-1.1-1.6-1.1-2.4 0-0.8-1.5-7.1-3.5-13.9-23.4-82.7-67.1-148.4-131-197.1-8.7-6.7-30-20.8-38.6-25.6-3.3-1.9-6.9-3.9-7.8-4.5-4.2-2.3-28.3-14.1-34.3-16.6-3.6-1.6-8.3-3.6-10.4-4.4-35.3-15.3-94.5-29.8-139.7-34.3-7.4-0.7-17.2-1.8-21.7-2.2-20.4-2.3-48.7-2.6-209.4-2.6-135.8 0-169.9 0.3-169.4 1zm330.7 43.3c33.8 2 54.6 4.6 78.9 10.5 74.2 17.6 126.4 54.8 164.3 117 3.5 5.8 18.3 36 20.5 42.1 10.5 28.3 15.6 45.1 20.1 67.3 1.1 5.4 2.6 12.6 3.3 16 0.7 3.3 1 6.4 0.7 6.7-0.5 0.4-100.9 0.6-223.3 0.5l-222.5-0.2-0.3-128.5c-0.1-70.6 0-129.3 0.3-130.4l0.4-1.9h71.1c39 0 78 0.4 86.5 0.9zm297.5 350.3c0.7 4.3 0.7 77.3 0 80.9l-0.6 2.7-227.5-0.2-227.4-0.3-0.2-42.4c-0.2-23.3 0-42.7 0.2-43.1 0.3-0.5 97.2-0.8 227.7-0.8h227.2zm-10.2 171.7c0.5 1.5-1.9 13.8-6.8 33.8-5.6 22.5-13.2 45.2-20.9 62-3.8 8.6-13.3 27.2-15.6 30.7-1.1 1.6-4.3 6.7-7.1 11.2-18 28.2-43.7 53.9-73 72.9-10.7 6.8-32.7 18.4-38.6 20.2-1.2 0.3-2.5 0.9-3 1.3-0.7 0.6-9.8 4-20.4 7.8-19.5 6.9-56.6 14.4-86.4 17.5-19.3 1.9-22.4 2-96.7 2h-76.9v-129.7-129.8l220.9-0.4c121.5-0.2 221.6-0.5 222.4-0.7 0.9-0.1 1.8 0.5 2.1 1.2z"/></svg>`;
+};
+
 
 interface InvoiceItem {
   id: string;
@@ -108,6 +119,7 @@ const numberToWords = (num: number, currency: string = 'AED'): string => {
 };
 
 const generateInvoicePDF = (data: InvoiceData, action: 'print' | 'download' = 'print') => {
+  const currencySvg = getCurrencySvg(data.currency || 'AED', '#000');
   const formattedDate = format(new Date(data.date), 'd-MMM-yy');
 
   // Dynamic layout variables to fit single page precisely when items are <= 8
@@ -621,7 +633,7 @@ const generateInvoicePDF = (data: InvoiceData, action: 'print' | 'download' = 'p
               <div class="brand-sub">${getLogoSubText(storeName)}</div>
             </div>
             <div class="header-right">
-              <svg width="48" height="48" viewBox="0 0 100 100" style="display: inline-block; vertical-align: middle;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 100 100" style="display: inline-block; vertical-align: middle;">
                 <circle cx="50" cy="50" r="40" stroke="#c22026" stroke-width="8.5" fill="none" stroke-dasharray="210 50" transform="rotate(-40 50 50)" />
                 <circle cx="50" cy="50" r="26" stroke="#c22026" stroke-width="4" fill="none" stroke-opacity="0.35" />
               </svg>
@@ -670,18 +682,29 @@ const generateInvoicePDF = (data: InvoiceData, action: 'print' | 'download' = 'p
           </div>
           
           <!-- Items Table -->
-          <table class="items-table">
+          <table class="items-table" style="table-layout: fixed; width: 100%;">
+            <colgroup>
+              <col style="width: 35px;" />
+              <col />
+              <col style="width: 70px;" />
+              <col style="width: 65px;" />
+              <col style="width: 35px;" />
+              <col style="width: 45px;" />
+              <col style="width: 75px;" />
+              <col style="width: 75px;" />
+              <col style="width: 95px;" />
+            </colgroup>
             <thead>
               <tr>
-                <th style="width: 50px; text-align: center;">SI<br/>No.</th>
+                <th style="width: 35px; text-align: center;">SI<br/>No.</th>
                 <th style="text-align: left;">Description of Goods</th>
-                <th style="width: 110px; text-align: right;">Quantity</th>
-                <th style="width: 80px; text-align: right;">Rate</th>
-                <th style="width: 60px; text-align: center;">per</th>
-                <th style="width: 60px; text-align: center;">VAT<br/>%</th>
-                <th style="width: 110px; text-align: right;">Amount</th>
-                <th style="width: 95px; text-align: right;">VAT<br/>(${uaeDirhamSvg})</th>
-                <th style="width: 125px; text-align: right;">Total<br/>Incl.VAT(${uaeDirhamSvg})</th>
+                <th style="width: 70px; text-align: right;">Quantity</th>
+                <th style="width: 65px; text-align: right;">Rate</th>
+                <th style="width: 35px; text-align: center;">per</th>
+                <th style="width: 45px; text-align: center;">VAT<br/>%</th>
+                <th style="width: 75px; text-align: right;">Amount</th>
+                <th style="width: 75px; text-align: right;">VAT<br/>(${currencySvg})</th>
+                <th style="width: 95px; text-align: right;">Total<br/>Incl.VAT(${currencySvg})</th>
               </tr>
             </thead>
             <tbody>
@@ -706,13 +729,13 @@ const generateInvoicePDF = (data: InvoiceData, action: 'print' | 'download' = 'p
                   <div style="margin-bottom: 15px;">
                     <span style="font-size: 9px; color: #555; display: block;">Amount Chargeable (in words)</span>
                     <span style="font-weight: bold; text-transform: uppercase; font-size: 10.5px; display: block; margin-top: 3px; line-height: 1.35; color: #000;">
-                      ${amountInWords} (${uaeDirhamSvg} ${formatNumber(totalNet)})
+                      ${amountInWords} (${currencySvg} ${formatNumber(totalNet)})
                     </span>
                   </div>
                   <div>
                     <span style="font-size: 9px; color: #555; display: block;">VAT Amount (in words)</span>
                     <span style="font-weight: bold; text-transform: uppercase; font-size: 10.5px; display: block; margin-top: 3px; line-height: 1.35; color: #000;">
-                      ${numberToWords(totalVat, data.currency)} (${uaeDirhamSvg} ${formatNumber(totalVat)})
+                      ${numberToWords(totalVat, data.currency)} (${currencySvg} ${formatNumber(totalVat)})
                     </span>
                   </div>
                 </td>
--- src/pages/invocie/config/InvoiceView.tsx
+++ src/pages/invocie/config/InvoiceView.tsx
@@ -8,11 +8,47 @@ import { ArrowLeft, Printer, Download } from "lucide-react";
 import generateInvoicePDF from './InvoicePrintTemplate';
 import { toast } from 'react-hot-toast';
 
-const UaeDirhamIcon = ({ className = "inline-block h-3 w-auto align-middle", style = {} }: { className?: string, style?: React.CSSProperties }) => (
-  <svg viewBox="0 0 1000 870" className={className} style={{ height: '11px', width: 'auto', display: 'inline-block', verticalAlign: 'middle', fill: 'currentColor', marginLeft: '2px', marginRight: '2px', ...style }}>
-    <path d="m88.3 1c0.4 0.6 2.6 3.3 4.7 5.9 15.3 18.2 26.8 47.8 33 85.1 4.1 24.5 4.3 32.2 4.3 125.6v87h-41.8c-38.2 0-42.6-0.2-50.1-1.7-11.8-2.5-24-9.2-32.2-17.8-6.5-6.9-6.3-7.3-5.9 13.6 0.5 17.3 0.7 19.2 3.2 28.6 4 14.9 9.5 26 17.8 35.9 11.3 13.6 22.8 21.2 39.2 26.3 3.5 1 10.9 1.4 37.1 1.6l32.7 0.5v43.3 43.4l-46.1-0.3-46.3-0.3-8-3.2c-9.5-3.8-13.8-6.6-23.1-14.9l-6.8-6.1 0.4 19.1c0.5 17.7 0.6 19.7 3.1 28.7 8.7 31.8 29.7 54.5 57.4 61.9 6.9 1.9 9.6 2 38.5 2.4l30.9 0.4v89.6c0 54.1-0.3 94-0.8 100.8-0.5 6.2-2.1 17.8-3.5 25.9-6.5 37.3-18.2 65.4-35 83.6l-3.4 3.7h169.1c101.1 0 176.7-0.4 187.8-0.9 19.5-1 63-5.3 72.8-7.4 3.1-0.6 8.9-1.5 12.7-2.1 8.1-1.2 21.5-4 40.8-8.9 27.2-6.8 52-15.3 76.3-26.1 7.6-3.4 29.4-14.5 35.2-18 3.1-1.8 6.8-4 8.2-4.7 3.9-2.1 10.4-6.3 19.9-13.1 4.7-3.4 9.4-6.7 10.4-7.4 4.2-2.8 18.7-14.9 25.3-21 25.1-23.1 46.1-48.8 62.4-76.3 2.3-4 5.3-9 6.6-11.1 3.3-5.6 16.9-33.6 18.2-37.8 0.6-1.9 1.4-3.9 1.8-4.3 2.6-3.4 17.6-50.6 19.4-60.9 0.6-3.3 0.9-3.8 3.4-4.3 1.6-0.3 24.9-0.3 51.8-0.1 53.8 0.4 53.8 0.4 65.7 5.9 6.7 3.1 8.7 4.5 16.1 11.2 9.7 8.7 8.8 10.1 8.2-11.7-0.4-12.8-0.9-20.7-1.8-23.9-3.4-12.3-4.2-14.9-7.2-21.1-9.8-21.4-26.2-36.7-47.2-44l-8.2-3-33.4-0.4-33.3-0.5 0.4-11.7c0.4-15.4 0.4-45.9-0.1-61.6l-0.4-12.6 44.6-0.2c38.2-0.2 45.3 0 49.5 1.1 12.6 3.5 21.1 8.3 31.5 17.8l5.8 5.4v-14.8c0-17.6-0.9-25.4-4.5-37-7.1-23.5-21.1-41-41.1-51.8-13-7-13.8-7.2-58.5-7.5-26.2-0.2-39.9-0.6-40.6-1.2-0.6-0.6-1.1-1.6-1.1-2.4 0-0.8-1.5-7.1-3.5-13.9-23.4-82.7-67.1-148.4-131-197.1-8.7-6.7-30-20.8-38.6-25.6-3.3-1.9-6.9-3.9-7.8-4.5-4.2-2.3-28.3-14.1-34.3-16.6-3.6-1.6-8.3-3.6-10.4-4.4-35.3-15.3-94.5-29.8-139.7-34.3-7.4-0.7-17.2-1.8-21.7-2.2-20.4-2.3-48.7-2.6-209.4-2.6-135.8 0-169.9 0.3-169.4 1zm330.7 43.3c33.8 2 54.6 4.6 78.9 10.5 74.2 17.6 126.4 54.8 164.3 117 3.5 5.8 18.3 36 20.5 42.1 10.5 28.3 15.6 45.1 20.1 67.3 1.1 5.4 2.6 12.6 3.3 16 0.7 3.3 1 6.4 0.7 6.7-0.5 0.4-100.9 0.6-223.3 0.5l-222.5-0.2-0.3-128.5c-0.1-70.6 0-129.3 0.3-130.4l0.4-1.9h71.1c39 0 78 0.4 86.5 0.9zm297.5 350.3c0.7 4.3 0.7 77.3 0 80.9l-0.6 2.7-227.5-0.2-227.4-0.3-0.2-42.4c-0.2-23.3 0-42.7 0.2-43.1 0.3-0.5 97.2-0.8 227.7-0.8h227.2zm-10.2 171.7c0.5 1.5-1.9 13.8-6.8 33.8-5.6 22.5-13.2 45.2-20.9 62-3.8 8.6-13.3 27.2-15.6 30.7-1.1 1.6-4.3 6.7-7.1 11.2-18 28.2-43.7 53.9-73 72.9-10.7 6.8-32.7 18.4-38.6 20.2-1.2 0.3-2.5 0.9-3 1.3-0.7 0.6-9.8 4-20.4 7.8-19.5 6.9-56.6 14.4-86.4 17.5-19.3 1.9-22.4 2-96.7 2h-76.9v-129.7-129.8l220.9-0.4c121.5-0.2 221.6-0.5 222.4-0.7 0.9-0.1 1.8 0.5 2.1 1.2z" />
-  </svg>
-);
+const getCompanyCurrency = (): string => {
+  try {
+    const userData = JSON.parse(localStorage.getItem('userData') || '{}');
+    return userData?.company_data?.currency || userData?.company_data?.currency_symbol || 'AED';
+  } catch {
+    return 'AED';
+  }
+};
+
+const resolveCurrency = (currencyStr?: string): string => {
+  const cur = (currencyStr || getCompanyCurrency()).toUpperCase();
+  if (cur.includes('INR') || cur.includes('₹') || cur.includes('RUPEE')) return 'INR';
+  if (cur.includes('USD') || cur.includes('$')) return 'USD';
+  return 'AED';
+};
+
+const CurrencyIcon = ({ className = "inline-block h-3 w-auto align-middle", style = {}, currency }: { className?: string, style?: React.CSSProperties, currency?: string }) => {
+  const resolved = resolveCurrency(currency);
+  if (resolved === 'INR') {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={className} style={{ height: '11px', width: 'auto', display: 'inline-block', verticalAlign: 'middle', fill: 'none', stroke: 'currentColor', strokeWidth: '2.5', strokeLinecap: 'round', strokeLinejoin: 'round', marginLeft: '2px', marginRight: '2px', ...style }}>
+        <path d="M6 3h12M6 8h12M6 13l8.5 8M6 13h3M9 13c6.667 0 6.667-10 0-10"/>
+      </svg>
+    );
+  }
+  if (resolved === 'USD') {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={className} style={{ height: '11px', width: 'auto', display: 'inline-block', verticalAlign: 'middle', fill: 'none', stroke: 'currentColor', strokeWidth: '2.5', strokeLinecap: 'round', strokeLinejoin: 'round', marginLeft: '2px', marginRight: '2px', ...style }}>
+        <line x1="12" y1="1" x2="12" y2="23"></line>
+        <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
+      </svg>
+    );
+  }
+  // Default AED (UAE Dirham)
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 870" className={className} style={{ height: '11px', width: 'auto', display: 'inline-block', verticalAlign: 'middle', fill: 'currentColor', marginLeft: '2px', marginRight: '2px', ...style }}>
+      <path d="m88.3 1c0.4 0.6 2.6 3.3 4.7 5.9 15.3 18.2 26.8 47.8 33 85.1 4.1 24.5 4.3 32.2 4.3 125.6v87h-41.8c-38.2 0-42.6-0.2-50.1-1.7-11.8-2.5-24-9.2-32.2-17.8-6.5-6.9-6.3-7.3-5.9 13.6 0.5 17.3 0.7 19.2 3.2 28.6 4 14.9 9.5 26 17.8 35.9 11.3 13.6 22.8 21.2 39.2 26.3 3.5 1 10.9 1.4 37.1 1.6l32.7 0.5v43.3 43.4l-46.1-0.3-46.3-0.3-8-3.2c-9.5-3.8-13.8-6.6-23.1-14.9l-6.8-6.1 0.4 19.1c0.5 17.7 0.6 19.7 3.1 28.7 8.7 31.8 29.7 54.5 57.4 61.9 6.9 1.9 9.6 2 38.5 2.4l30.9 0.4v89.6c0 54.1-0.3 94-0.8 100.8-0.5 6.2-2.1 17.8-3.5 25.9-6.5 37.3-18.2 65.4-35 83.6l-3.4 3.7h169.1c101.1 0 176.7-0.4 187.8-0.9 19.5-1 63-5.3 72.8-7.4 3.1-0.6 8.9-1.5 12.7-2.1 8.1-1.2 21.5-4 40.8-8.9 27.2-6.8 52-15.3 76.3-26.1 7.6-3.4 29.4-14.5 35.2-18 3.1-1.8 6.8-4 8.2-4.7 3.9-2.1 10.4-6.3 19.9-13.1 4.7-3.4 9.4-6.7 10.4-7.4 4.2-2.8 18.7-14.9 25.3-21 25.1-23.1 46.1-48.8 62.4-76.3 2.3-4 5.3-9 6.6-11.1 3.3-5.6 16.9-33.6 18.2-37.8 0.6-1.9 1.4-3.9 1.8-4.3 2.6-3.4 17.6-50.6 19.4-60.9 0.6-3.3 0.9-3.8 3.4-4.3 1.6-0.3 24.9-0.3 51.8-0.1 53.8 0.4 53.8 0.4 65.7 5.9 6.7 3.1 8.7 4.5 16.1 11.2 9.7 8.7 8.8 10.1 8.2-11.7-0.4-12.8-0.9-20.7-1.8-23.9-3.4-12.3-4.2-14.9-7.2-21.1-9.8-21.4-26.2-36.7-47.2-44l-8.2-3-33.4-0.4-33.3-0.5 0.4-11.7c0.4-15.4 0.4-45.9-0.1-61.6l-0.4-12.6 44.6-0.2c38.2-0.2 45.3 0 49.5 1.1 12.6 3.5 21.1 8.3 31.5 17.8l5.8 5.4v-14.8c0-17.6-0.9-25.4-4.5-37-7.1-23.5-21.1-41-41.1-51.8-13-7-13.8-7.2-58.5-7.5-26.2-0.2-39.9-0.6-40.6-1.2-0.6-0.6-1.1-1.6-1.1-2.4 0-0.8-1.5-7.1-3.5-13.9-23.4-82.7-67.1-148.4-131-197.1-8.7-6.7-30-20.8-38.6-25.6-3.3-1.9-6.9-3.9-7.8-4.5-4.2-2.3-28.3-14.1-34.3-16.6-3.6-1.6-8.3-3.6-10.4-4.4-35.3-15.3-94.5-29.8-139.7-34.3-7.4-0.7-17.2-1.8-21.7-2.2-20.4-2.3-48.7-2.6-209.4-2.6-135.8 0-169.9 0.3-169.4 1zm330.7 43.3c33.8 2 54.6 4.6 78.9 10.5 74.2 17.6 126.4 54.8 164.3 117 3.5 5.8 18.3 36 20.5 42.1 10.5 28.3 15.6 45.1 20.1 67.3 1.1 5.4 2.6 12.6 3.3 16 0.7 3.3 1 6.4 0.7 6.7-0.5 0.4-100.9 0.6-223.3 0.5l-222.5-0.2-0.3-128.5c-0.1-70.6 0-129.3 0.3-130.4l0.4-1.9h71.1c39 0 78 0.4 86.5 0.9zm297.5 350.3c0.7 4.3 0.7 77.3 0 80.9l-0.6 2.7-227.5-0.2-227.4-0.3-0.2-42.4c-0.2-23.3 0-42.7 0.2-43.1 0.3-0.5 97.2-0.8 227.7-0.8h227.2zm-10.2 171.7c0.5 1.5-1.9 13.8-6.8 33.8-5.6 22.5-13.2 45.2-20.9 62-3.8 8.6-13.3 27.2-15.6 30.7-1.1 1.6-4.3 6.7-7.1 11.2-18 28.2-43.7 53.9-73 72.9-10.7 6.8-32.7 18.4-38.6 20.2-1.2 0.3-2.5 0.9-3 1.3-0.7 0.6-9.8 4-20.4 7.8-19.5 6.9-56.6 14.4-86.4 17.5-19.3 1.9-22.4 2-96.7 2h-76.9v-129.7-129.8l220.9-0.4c121.5-0.2 221.6-0.5 222.4-0.7 0.9-0.1 1.8 0.5 2.1 1.2z"/>
+    </svg>
+  );
+};
+
 
 // Using SalesInvoice type from service
 
@@ -40,10 +76,11 @@ export default function InvoiceView() {
         } else {
           setError('Invoice not found');
         }
-      } catch (err: any) {
+      } catch (err) {
+        const errorMsg = err instanceof Error ? err.message : 'Unknown error';
         console.error('Error fetching invoice:', err);
-        setError(`Failed to fetch invoice: ${err.message || 'Unknown error'}`);
-        toast.error(`Failed to fetch invoice: ${err.message || 'Unknown error'}`);
+        setError(`Failed to fetch invoice: ${errorMsg}`);
+        toast.error(`Failed to fetch invoice: ${errorMsg}`);
       } finally {
         setLoading(false);
       }
@@ -149,7 +186,7 @@ export default function InvoiceView() {
       }),
       date: invoice.invoiceDate,
       status: 'pending' as const,
-      currency: 'AED',
+      currency: getCompanyCurrency(),
     };
   };
 
@@ -234,7 +271,7 @@ export default function InvoiceView() {
       const month = months[d.getMonth()];
       const year = String(d.getFullYear()).slice(-2);
       return `${day}-${month}-${year}`;
-    } catch (e) {
+    } catch {
       return dateString;
     }
   };
@@ -389,7 +426,7 @@ export default function InvoiceView() {
                 </div>
               </div>
               <div className="flex-1 flex justify-end">
-                <svg width="48" height="48" viewBox="0 0 100 100">
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 100 100">
                   <circle cx="50" cy="50" r="40" stroke="#c22026" strokeWidth="8.5" fill="none" strokeDasharray="210 50" transform="rotate(-40 50 50)" />
                   <circle cx="50" cy="50" r="26" stroke="#c22026" strokeWidth="4" fill="none" stroke-opacity="0.35" />
                 </svg>
@@ -441,25 +478,36 @@ export default function InvoiceView() {
 
             {/* Table Items */}
             <div className="overflow-x-auto w-full">
-              <table className="w-full border-collapse border border-black text-xs">
+              <table className="w-full border-collapse border border-black text-xs table-fixed">
+                <colgroup>
+                  <col style={{ width: '35px' }} />
+                  <col />
+                  <col style={{ width: '70px' }} />
+                  <col style={{ width: '65px' }} />
+                  <col style={{ width: '35px' }} />
+                  <col style={{ width: '45px' }} />
+                  <col style={{ width: '75px' }} />
+                  <col style={{ width: '75px' }} />
+                  <col style={{ width: '95px' }} />
+                </colgroup>
                 <thead>
                   <tr className="bg-gray-100 font-bold text-[11px] border-b border-black text-black">
-                    <th className="border border-black p-1 text-center w-12">SI<br/>No.</th>
+                    <th className="border border-black p-1 text-center w-[35px]">SI<br/>No.</th>
                     <th className="border border-black p-1 text-left">Description of Goods</th>
-                    <th className="border border-black p-1 text-right w-28">Quantity</th>
-                    <th className="border border-black p-1 text-right w-20">Rate</th>
-                    <th className="border border-black p-1 text-center w-16">per</th>
-                    <th className="border border-black p-1 text-center w-16">VAT<br/>%</th>
-                    <th className="border border-black p-1 text-right w-28">Amount</th>
-                    <th className="border border-black p-1 text-right w-24">VAT<br/>(<UaeDirhamIcon />)</th>
-                    <th className="border border-black p-1 text-right w-32">Total<br/>Incl.VAT(<UaeDirhamIcon />)</th>
+                    <th className="border border-black p-1 text-right w-[70px]">Quantity</th>
+                    <th className="border border-black p-1 text-right w-[65px]">Rate</th>
+                    <th className="border border-black p-1 text-center w-[35px]">per</th>
+                    <th className="border border-black p-1 text-center w-[45px]">VAT<br/>%</th>
+                    <th className="border border-black p-1 text-right w-[75px]">Amount</th>
+                    <th className="border border-black p-1 text-right w-[75px]">VAT<br/>(<CurrencyIcon currency={getCompanyCurrency()} />)</th>
+                    <th className="border border-black p-1 text-right w-[95px]">Total<br/>Incl.VAT(<CurrencyIcon currency={getCompanyCurrency()} />)</th>
                   </tr>
                 </thead>
                 <tbody>
                   {invoice.items.map((item, index) => {
                     const itemName = typeof item.item === 'object' && item.item !== null
-                      ? item.item.name
-                      : 'Unknown Item';
+                       ? item.item.name
+                       : 'Unknown Item';
                     const vatVal = item.vat || 0;
                     const grossAmount = item.quantity * item.unitPrice;
                     const vatAmount = grossAmount * (vatVal / 100);
@@ -511,13 +559,13 @@ export default function InvoiceView() {
                         <div>
                           <span className="text-gray-600 block text-[10px]">Amount Chargeable (in words)</span>
                           <span className="font-bold text-black uppercase block mt-1 leading-snug">
-                            {numberToWords(finalAmount, 'AED')} (<UaeDirhamIcon /> {finalAmount.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })})
+                            {numberToWords(finalAmount, getCompanyCurrency())} (<CurrencyIcon currency={getCompanyCurrency()} /> {finalAmount.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })})
                           </span>
                         </div>
                         <div>
                           <span className="text-gray-600 block text-[10px]">VAT Amount (in words)</span>
                           <span className="font-bold text-black uppercase block mt-1 leading-snug">
-                            {numberToWords(totalVat, 'AED')} (<UaeDirhamIcon /> {totalVat.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })})
+                            {numberToWords(totalVat, getCompanyCurrency())} (<CurrencyIcon currency={getCompanyCurrency()} /> {totalVat.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })})
                           </span>
                         </div>
                       </div>
